b96a32ff0fa7a5b03254f17813066cfcd9b30dcb
박성민 <psm7335@naver.com>
2022-08-08 23:04:20 +0900

    setting: project init

---
+++ src/main/java/com/csbreaker/springwil/SpringwilApplication.java
@@ -0,0 +1,13 @@
+package com.csbreaker.springwil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwilApplication.class, args);
+	}
+
+}
